894b5c5bda750de72f2e49bc190b0822f24fa536
defaultuser0 <axolotlslmao@gmail.com>
2024-12-29 16:48:16 -0600

    yes

---
+++ kysfog/Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime;
+using Photon;
+using MelonLoader;
+using CapuchinTemplate;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(ModCodeThingieYea), ModInfo.Name, ModInfo.Description, ModInfo.Version, ModInfo.Author)]
+[assembly: MelonGame("Duttbust", "Capuchin")]
+
+namespace CapuchinTemplate
+{
+    public class ModCodeThingieYea : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
+        {
+            if (sceneName == "CapuchinCopy")
+            {
+                GameObject.Find("Global/Levels/Forest/Fog").SetActive(false);
+            }
+        }
+    }
+}
